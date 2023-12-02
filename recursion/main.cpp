d05de95924a12c1f0a71d324de95b6ba4eb559a9
Kyan Mulji <muljikyan@gmail.com>
2023-12-02 01:00:18 -0500

    first commit

---
+++ recursion/main.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+void fun1(int n)
+{
+    if (n == 0)
+    {
+
+        return;
+    }
+
+    std::cout << n << std::endl;
+    fun1(n - 1);
+}
+int fun2(int n)
+{
+    static int x = 0;
+    if (n == 0)
+    {
+        return 0;
+    }
+    x++;
+    return fun2(n - 1) + x;
+}
+/*
+    Write a recursive function to calculate the sum of
+    the first n numbers;
+*/
+int sum(int n)
+{
+    if (n == 0)
+        return 0;
+    
+    return sum(n - 1) + n;
+}
+/*
+    Write a recursive function to calculate the factorial of n.
+*/
+int fact(int n){
+    if(n<=0){
+        return 1;
+    }
+    return fact(n-1)*n;
+}
+int main()
+{
+    std::cout<<fact(3)<<std::endl;
+    return 0;
+}
