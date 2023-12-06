059a9a437809d199048592a168636f2a3fee6f0b
KMulji <muljikyan@gmail.com>
2023-12-06 20:00:29 +0300

    recursion

--- recursion/main.cpp
+++ recursion/main.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <unordered_map>
+using std::unordered_map;
 void fun1(int n)
 {
     if (n == 0)
@@ -28,20 +30,100 @@ int sum(int n)
 {
     if (n == 0)
         return 0;
-    
+
     return sum(n - 1) + n;
 }
 /*
     Write a recursive function to calculate the factorial of n.
 */
-int fact(int n){
-    if(n<=0){
+int fact(int n)
+{
+    if (n <= 0)
+    {
         return 1;
     }
-    return fact(n-1)*n;
+    return fact(n - 1) * n;
+}
+/*
+    Write a recursive function to calculate power m^n
+*/
+int pow(int m, int n)
+{
+    if (n == 0)
+    {
+        return 1;
+    }
+    return pow(m, n - 1) * m;
+}
+/*Write a recursive function for taylor series of e^x*/
+double e(int x, int n)
+{
+    static int p = 0;
+    static int f = 1;
+    double r;
+    if (n == 0)
+    {
+        return 1;
+    }
+
+    r = e(x, n - 1);
+    p = p * x;
+    f = f * n;
+    return r + p / f;
+}
+/*Write a iterative and recursive function for taylor series with horners rule optimization*/
+double eh(int x, int n)
+{
+    double s = 1;
+    for (; n > 0; n--)
+    {
+        s = 1 + x / n * s;
+    }
+    return s;
+}
+double ehr(int x, int n)
+{
+    static int s = 1;
+
+    if (n == 0)
+    {
+        return s;
+    }
+    s = 1 + x / n * s;
+
+    return e(x, n - 1);
+}
+int fib(int n)
+{
+    static unordered_map<int, int> mp;
+    if (n == 0)
+    {
+        mp[n] = 0;
+    }
+    else if (n == 1)
+    {
+        mp[n] = 1;
+    }
+    else
+    {
+        mp[n] = fib(n - 2) + fib(n - 1);
+    }
+
+    return mp[n];
+}
+int iterativeFib(int n){
+    int f0=0;
+    int f1=1;
+    int sum;
+    for(int i=2;i<=n;i++){
+        sum=f0+f1;
+        f0=f1;
+        f1=sum;
+    }
+    return sum;
 }
 int main()
 {
-    std::cout<<fact(3)<<std::endl;
+    std::cout << iterativeFib(10) << std::endl;
     return 0;
 }
